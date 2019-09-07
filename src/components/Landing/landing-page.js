dcca7e2bafed24e671be47dbfad88fa7def39c1e
shubham-shinde <shindes0321@gmail.com>
2019-09-08 03:35:45 +0530

    land card added

--- src/components/Landing/landing-page.js
+++ src/components/Landing/landing-page.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './landing-page.scss';
 import Icons from '../../services/icon-service';
+import LandCard from '../common/landCard/landCard';
 const FaSearch = Icons['fa-search'];
 
 
@@ -13,6 +14,7 @@ class LandingPage extends React.Component {
                 Made by shubham
                 icon example
                 <FaSearch/>
+                <LandCard/>
             </div>
         )
     }
---
+++ src/components/common/landCard/landCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './land-card.scss';
+
+
+class LandCard extends React.Component {
+    render() {
+        return (
+            <div className="land-card">
+                this is a card
+            </div>
+        )
+    }
+}
+
+LandCard.propTypes = {
+
+}
+
+export default LandCard
