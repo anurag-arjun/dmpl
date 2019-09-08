4f60fd7fc42958cc7d74800f93e2243143f4597c
shivam1534 <shivam.mkp@gmail.com>
2019-09-08 13:06:03 +0530

    firstcard

--- src/components/common/landCard/landCard.js
+++ src/components/common/landCard/landCard.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './land-card.scss';
-
+import cardImg from '../assets/images/img.png'
 
 class LandCard extends React.Component {
     render() {
         return (
             <div className="land-card">
-                this is a card
+                    <img src={cardImg}></img>
+                    <div className='cc'>
+                         <p className="p1">Double Parsel</p>
+                         <p className="p2">Aquire 9 days ago</p>
+                      </div>   
+                    <div className="dv"> 
+                        <i class="land-icon"></i>
+                        <p>2 LAND</p>
+                    </div>
             </div>
         )
     }
