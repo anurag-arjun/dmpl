9b9abfd5c1eda2e7cea6e19aaece4ed918855cd5
Shivam Srivastava <shivam.mkp@gmail.com>
2019-09-10 20:21:15 +0530

    colorKey Added

--- src/components/colorKey/colorKey.js
+++ src/components/colorKey/colorKey.js
@@ -3,7 +3,50 @@ import './colorKey.scss';
 
 class colorKey extends React.Component {
 	render() {
-		return <div></div>;
+		return <div className="colorKey">
+			       <h1 className="colorheading">Color Key</h1>
+
+                    <div className="colorKey-box">
+						
+						<div className="colorKey-box-row">
+							<div className="colorKey-box-row-col11">
+							<span className='colorKey-box-row-col11-c1'></span> <p>Yours</p>
+							</div>
+							<div className="colorKey-box-row-col12">
+							<span className='colorKey-box-row-col12-c2'></span> <p>Yours on sale</p>
+							</div>
+							<div className="colorKey-box-row-col13">
+							<span className='colorKey-box-row-col13-c3'></span> <p>Taken</p>
+							</div>
+						</div>
+
+						<div className="colorKey-box-row">
+							<div className="colorKey-box-row-col21">
+							<span className='colorKey-box-row-col21-c1'></span> <p>District</p>
+							</div>
+							<div className="colorKey-box-row-col22">
+							<span className='colorKey-box-row-col22-c2'></span> <p>Contribution</p>
+							</div>
+							<div className="colorKey-box-row-col23">
+							<span className='colorKey-box-row-col23-c3'></span>< p>Roads</p>
+							</div>
+						</div>
+
+						<div className="colorKey-box-row">
+							<div className="colorKey-box-row-col31">
+							<span className='colorKey-box-row-col31-c1'></span> <p>Plaza</p>
+							</div>
+							<div className="colorKey-box-row-col32">
+							<span className='colorKey-box-row-col32-c2'></span><p>Unowned</p>
+							</div>
+							<div className="colorKey-box-row-col33">
+							<span className='colorKey-box-row-col33-c3'></span><p>On sale</p>
+							</div>
+						</div>
+					
+
+					</div>
+		      </div>;
 	}
 }
 
