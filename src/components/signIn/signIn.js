1fdbafa0376ed136733cc54eee7c3381d4fc824e
Shivam Srivastava <shivam.mkp@gmail.com>
2019-09-10 10:55:13 +0530

    SIgn in DOne!!

--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './signIn.scss';
 
 class signIn extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="av">abc</div>
+				<div className="frame">
+					<img src="https://i.ibb.co/2n3b5Sj/pic1.png"></img>
+					<h1 id='head'> Get Started</h1>
+					<p id="para">First off you’ll need a safe place to keep all your LAND. <br></br> 
+					You can use the <span className='tagg'>MetaMask</span>  extension or a hardware wallet <br></br>
+					 like <span className='tagg'>Ledger Nano S.</span> </p>
+					<button id='btn'>Connect</button>
+				</div>
 			</div>
 		);
 	}
