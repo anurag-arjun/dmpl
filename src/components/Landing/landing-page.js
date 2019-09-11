7f18d8aa190c2eded974b4b1bf58cb1c38509ce8
EvilSpark <kamrantahir25@gmail.com>
2019-09-12 03:00:37 +0530

    Marketplace page done, pagination left, .prettier and other config file updated

--- src/components/Landing/landing-page.js
+++ src/components/Landing/landing-page.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import './landing-page.scss';
 import Icons from '../../services/icon-service';
 import LandCard from '../common/landCard/landCard';
@@ -8,38 +8,38 @@ const FaSearch = Icons['fa-search'];
 import Header from '../Header/Header.js';
 
 class LandingPage extends React.Component {
-	render() {
-		const {maps} = this.props;
-		console.log(maps);
+  render() {
+    const { maps } = this.props;
+    console.log(maps);
 
-		return (
-			<div className="landing-page">
-				<Header />
-				<div className="landing-page-publications">
-					<div className="landing-page-header">
-						<h3 className="landing-page-p">Newest LAND</h3>
-						<a href="/marketplace" className="landing-page-a">
-							VIEW MORE
-						</a>
-					</div>
-					<div className="landing-page-scroller">
-						{maps.map((e, i) => (
-							<LandCard {...e} index={i} />
-						))}
-					</div>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="landing-page">
+        <Header />
+        <div className="landing-page-publications">
+          <div className="landing-page-header">
+            <h3 className="landing-page-p">Newest LAND</h3>
+            <a href="/marketplace" className="landing-page-a">
+              VIEW MORE
+            </a>
+          </div>
+          <div className="landing-page-scroller">
+            {maps.map((e, i) => (
+              <LandCard {...e} index={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 LandingPage.propTypes = {};
 
-const mapStateToProps = state => {
-	const top10 = state.maps.map.slice(0, 10);
-	return {
-		maps: top10
-	};
+const mapStateToProps = (state) => {
+  const top10 = state.maps.map.slice(0, 10);
+  return {
+    maps: top10,
+  };
 };
 
 export default connect(mapStateToProps)(LandingPage);
--- src/components/Marketplace/Marketplace.js
+++ src/components/Marketplace/Marketplace.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import './marketplace.scss';
+import LandCard from '../common/landCard/landCard';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Marketplace extends React.Component {
-	render() {
-		return (
-			<div className="marketplace-container">
-				<div className="marketplace-container-menu">
-					<div className="menu-container">
-						<div className="menu-container-item">
-							<a href="" className="menu-item active">
-								Parcels
-							</a>
-							<a href="" className="menu-item">
-								Estates
-							</a>
-						</div>
-						<div className="menu-list">
-							<input type="text" list="lang" placeholder="English"></input>
-							<datalist id="lang">
-								<option value="English"></option>
-								<option value="French"></option>
-								<option value="Japanese"></option>
-								<option value="Spanish"></option>
-							</datalist>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { maps } = this.props;
+
+    return (
+      <div className="marketplace-container">
+        <div className="marketplace-container-menu">
+          <div className="menu-container">
+            <div className="menu-container-item">
+              <a href="" className="menu-item active">
+                Parcels
+              </a>
+              <a href="" className="menu-item">
+                Estates
+              </a>
+            </div>
+            <div className="menu-list">
+              <input type="text" list="lang" placeholder="English"></input>
+              <datalist id="lang">
+                <option value="English"></option>
+                <option value="French"></option>
+                <option value="Japanese"></option>
+                <option value="Spanish"></option>
+              </datalist>
+            </div>
+          </div>
+        </div>
+        <div className="marketplace-container-cards-collection">
+          <div className="marketplace-container-cards">
+            {maps.map((e, i) => (
+              <LandCard {...e} index={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Marketplace;
+Marketplace.propTypes = {};
+
+const mapStateToProps = (state) => {
+  const top12 = state.maps.map.slice(0, 12);
+  return {
+    maps: top12,
+  };
+};
+
+export default connect(mapStateToProps)(Marketplace);
