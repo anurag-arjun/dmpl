a5ea92ed8eadee6c85e21d0ff5646eee44db0550
Shivam Srivastava <shivam.mkp@gmail.com>
2019-09-18 01:09:22 +0530

    editing html of Connect wallet

--- src/components/connectWallet/connectWallet.js
+++ src/components/connectWallet/connectWallet.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './connectWallet.scss'
+import wallet from "../common/assets/fonts/wallet.svg"
 
 class ConnectWallet extends React.Component {
   render () {
     return (
       <div>
-        <p className='Component'>abc</p>
+        <div className="connect-wallet">
+          <div className="wallet-edge">
+            <img src={wallet}></img>
+          </div>
+          <div className="wallet-text">
+            <h1>Connect your Wallet</h1>
+            <p>First of all, you need a safe placero keep your land. Choose to <br></br> connect wallet by following connection</p>
+          </div>
+          <span>CONNECT WITH</span>
+          <div>
+
+          </div>
+        </div>
       </div>
     )
   }
