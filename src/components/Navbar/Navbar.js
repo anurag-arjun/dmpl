2a38c3a02b121bbe5de1cc38c8f79d71bc814bec
EvilSpark <kamrantahir25@gmail.com>
2019-09-19 02:58:31 +0530

    Navbar logic fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,50 +6,75 @@ import { connect } from 'react-redux';
 import MaticIcon from '../common/assets/images/blue_dark.svg';
 import ProfileIcon from '../common/assets/images/square.png';
 
+//React Icons Start
+
 const FaBars = icons['fa-bars'];
 const FaBell = icons['fa-bell'];
 
+//Icons End
+
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topNav: true,
+      navSmall: false,
+    };
+  }
+
+  navBarClickHandler = () => {
+    const newState = { ...this.state, topNav: !this.state.topNav };
+    console.log(newState);
+    this.setState(newState);
+  };
+
   render() {
     const { market } = this.props;
+
     const isSignIn = true;
     return (
-      <nav className="Navigation">
-        <a href="/">
-          {' '}
-          <img src={logo} alt="logo" className="Navigation-logo" />
-        </a>
-        <div className="Navigation-menu">
-          {/* <a href="/atlas" className="Navigation-item">
-            Atlas
-          </a> */}
-          {market && (
-            <a href="/marketplace" className="Navigation-item enabled">
-              Marketplace
-            </a>
-          )}
-        </div>
-        {!isSignIn ? (
-          <a href="/signin" className="Navigation-item">
-            SignIn
+      <div>
+        <nav
+          className={
+            this.state.topNav ? 'Navigation' : 'Navigation Responsive'
+          }>
+          <FaBars className="FaBars" onClick={this.navBarClickHandler} />
+          <a href="/">
+            {' '}
+            <img src={logo} alt="logo" className="Navigation-logo" />
           </a>
-        ) : (
-          <div className="signedIn">
-            <img src={MaticIcon} alt="MaticIcon" className="signedIn-icon" />
-            <a href="/" className="signedIn-link">
-              SWITCH TO MATIC
+          <div className="Navigation-menu">
+            <a href="/atlas" className="Navigation-item">
+              Atlas
             </a>
-            <FaBell className="signedIn-bell" />
-            <p className="signedIn-p">⏣ &nbsp; 500</p>
-            <img
-              src={ProfileIcon}
-              alt="ProfileIcon"
-              className="signedIn-profile"
-              style={{ width: '40px', height: 'auto' }}
-            />
+            {market && (
+              <a href="/marketplace" className="Navigation-item enabled">
+                Marketplace
+              </a>
+            )}
           </div>
-        )}
-      </nav>
+          {!isSignIn ? (
+            <a href="/signin" className="Navigation-item">
+              SignIn
+            </a>
+          ) : (
+            <div className="signedIn">
+              <img src={MaticIcon} alt="MaticIcon" className="signedIn-icon" />
+              <a href="/" className="signedIn-link">
+                SWITCH TO MATIC
+              </a>
+              <FaBell className="signedIn-bell" />
+              <p className="signedIn-p">⏣ &nbsp; 500</p>
+              <img
+                src={ProfileIcon}
+                alt="ProfileIcon"
+                className="signedIn-profile"
+                style={{ width: '40px', height: 'auto' }}
+              />
+            </div>
+          )}
+        </nav>{' '}
+      </div>
     );
   }
 }
