c7e1e600a1fb3481abbab634b0d8645e8a47d1e4
EvilSpark <kamrantahir25@gmail.com>
2019-09-21 04:07:41 +0530

    Loader added in both landing and marketplace. Fixed margin width of land-card big

--- src/components/Landing/landing-page.js
+++ src/components/Landing/landing-page.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'
-import * as actions from '../../actions/initActions'
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions/initActions';
 import './landing-page.scss';
 import Icons from '../../services/icon-service';
 import LandCard from '../common/landCard/landCard';
 import Header from '../Header/Header.js';
 import NavBar from '../Navbar/Navbar';
+import Loader from '../common/assets/images/Loader.svg';
 const FaSearch = Icons['fa-search'];
-const FaChevron =Icons['fa-chevron-right'];
+const FaChevron = Icons['fa-chevron-right'];
 
 class LandingPage extends React.Component {
   componentWillMount = () => {
     this.props.actions.initCards();
-  }
+  };
   render() {
     const { cards } = this.props;
-    console.log(cards);
-
     return (
       <div className="landing-page">
         {/* <NavBar /> */}
@@ -27,14 +26,23 @@ class LandingPage extends React.Component {
           <div className="landing-page-header">
             <h3 className="landing-page-p">Newest LAND</h3>
             <a href="/marketplace" className="landing-page-a">
-              VIEW MORE 
-              <span><FaChevron/></span>
+              VIEW MORE
+              <span>
+                <FaChevron />
+              </span>
             </a>
           </div>
           <div className="landing-page-scroller">
-            {cards.map((e, i) => (
+            {this.props.isFetching ? (
+              <img src={Loader} alt="Loading" className="Loader" />
+            ) : (
+              cards.map((e, i) => <LandCard {...e} index={i} normal />)
+            )}
+            {/* {!this.state.isLoading &&
+              } */}
+            {/* {cards.map((e, i) => (
               <LandCard {...e} index={i} normal />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
@@ -46,13 +54,18 @@ LandingPage.propTypes = {};
 
 const mapStateToProps = (state) => {
   const top10 = state.cards.cards.slice(0, 10);
+  const isFetching = state.cards.isFetching;
   return {
     cards: top10,
+    isFetching,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(actions, dispatch)
-})
+  actions: bindActionCreators(actions, dispatch),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(LandingPage);
