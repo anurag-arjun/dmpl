c07c0c0debf29175b360d9f0050549187d212ebf
Shivam Srivastava <shivam.mkp@gmail.com>
2019-09-21 12:43:43 +0530

    connect wallet connected

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -3,8 +3,20 @@ import './Wallet.scss'
 import wallet from '../common/assets/images/square.png'
 import blue_dark from '../common/assets/images/blue_dark.svg'
 import balance from '../common/assets/images/balance-icon.svg'
+import icons from '../../services/icon-service';
+const StartIcons = icons['md-start-circle'];
 import NavBar from '../Navbar/Navbar'
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      whymatic:false
+    };
+  }
+    whymaticHandler= ()=>{
+      this.setState({...this.state, whymatic: !this.state.whymatic});
+    };
+
   render () {
     return (
       <div>
@@ -32,7 +44,10 @@ class Wallet extends React.Component {
                   <div className='button1'><a href="#">Buy More</a></div>
                   <button className="button2"><a href="#"><div>TRANSFER</div></a></button>
                   <button className="button3">
-                    <img src={blue_dark} /> <span><a href="#">ADD FUND TO MATIC</a></span>
+                    <img src={blue_dark} /> 
+                    <span>
+                      <a onClick={this.whymaticHandler}>ADD FUND TO MATIC</a>
+                    </span>
                   </button>
                 </div>
               </span>
@@ -122,6 +137,85 @@ class Wallet extends React.Component {
               </div>
             </div>
           </div>
+          {
+            this.state.whymatic&&
+            <div className="why">
+                    <div className="appendix-1">
+                        <div className="why-matic">
+                            <h1>Why Matic ?</h1>
+                            <span>LEARN MORE</span>
+                        </div>
+                        <div className="why-row">
+                            <div className="why-row-col-1">
+                                <h2>Advantage over Mainnet</h2>
+                                <div>
+                                    <span>
+                                        <StartIcons size={40} className="col-1-icon" />
+                                    </span>
+                                    <div>
+                                        <h2>Faster transactions</h2>
+                                        <p>Make near-instant transactions on Matic Network</p>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <span>
+                                        <StartIcons size={40} className="col-1-icon" />
+                                    </span>
+                                    <div>
+                                        <h2>Fees</h2>
+                                        <p>Negligible gas fees</p>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <span><StartIcons size={40} className="col-1-icon" /></span>
+                                    <div>
+                                        <h2>Bulk Deposits</h2>
+                                        <p>Deposit multiple assests in one-go</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="why-row-col-2">
+                                <h2>How it works ?</h2>
+
+                                <div>
+                                    <div>
+                                        <div>
+                                            <div>
+                                                <span>1</span>
+                                            </div>
+                                            <div className="line"></div>
+                                        </div>
+                                        <p>Move to Matic</p>
+                                    </div>
+                                </div>
+
+                                <div>
+                                        <div>
+                                            <div>
+                                                <span>2</span>
+                                            </div>
+                                            <p>Sell and Trade on Matic</p>
+                                        </div>
+                                       
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <div><span>3</span></div>
+                                            <p>Move Back to Mainnet</p>
+                                        </div>
+                                    </div>
+                            </div>
+                          
+                        </div>
+                        <div className="why-row-2">
+                                <button className="why-row-2-btn"> <a href="#"> NEXT</a> </button>
+                            </div>
+                    </div>
+                </div>
+          }
         </div>
       </div>
     )
