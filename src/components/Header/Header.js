5c57773b000d5c344bb83ea0f9a013db3de6d9e7
Shivam Srivastava <shivam.mkp@gmail.com>
2019-09-21 11:10:01 +0530

    hearder icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import logo from '../common/assets/images/nav-logo.svg';
 import './Header.scss'
 
 class Header extends React.Component {
@@ -7,9 +7,12 @@ class Header extends React.Component {
     return (
       <div className='Header-main'>
         <div className='Header-Heading'>
+          <div>
+            <span className="logo"><img src={logo}></img></span>
           <h1 className='Header-Heading-light'>
             Choose your ideal location in Genesis City
           </h1>
+          </div>
           <p className='Header-Heading-dark'>Decentralized market for LAND</p>
           <button className='Header-button button-primary'><a href="#">GET STARTED</a></button>
           <a
